7e228f8c4e8214bd0488cbec8c0de81d04c1bd89
feredean <tiberiu.feredean@gmail.com>
2016-11-02 11:29:12 +0200

    update framework

--- src/ios/CDVMC.m
+++ src/ios/CDVMC.m
@@ -14,48 +14,48 @@
     self.command = command;
     NSString *realname = command.arguments[0];
     NSString *mobile = command.arguments[1];
-    
+
     NSDictionary* userInfo =  @{
                                 @"name"     : realname,
                                 @"tel"       : mobile,
                                 };
-    
-    
-    [MQManager initWithAppkey:@"cab23cf1f964306751d12b8eb29f8959" completion:^(NSString *clientId, NSError *error) {
+
+
+    [MQManager initWithAppkey:@"a548e3b25552571820ac28fe1b9b5505" completion:^(NSString *clientId, NSError *error) {
         if (!error) {
             NSLog(@"美洽 SDK：初始化成功:%@", clientId);
-            
+
             MQChatViewManager *chatViewManager = [[MQChatViewManager alloc] init];
             [chatViewManager setClientInfo:userInfo];
-            
+
             [chatViewManager enableSyncServerMessage:false];
-            
+
             [chatViewManager.chatViewStyle setEnableOutgoingAvatar:false];
             [chatViewManager.chatViewStyle setEnableRoundAvatar:YES];
             [chatViewManager.chatViewStyle setNavBarTintColor:[UIColor whiteColor]];
             [chatViewManager.chatViewStyle setNavTitleColor:[UIColor whiteColor]];
             [chatViewManager.chatViewStyle setNavBarColor:[UIColor colorWithRed:0.941 green:0.353 blue:0.314 alpha:1.0]];
             [chatViewManager.chatViewStyle setEnableOutgoingAvatar:YES];
-            
+
             [chatViewManager pushMQChatViewControllerInViewController:self.viewController];
-            
-            
+
+
             // [chatViewManager setScheduleLogicWithRule:MQChatScheduleRulesRedirectEnterprise];
-            
+
             // [chatViewManager.chatViewStyle setStatusBarStyle: UIStatusBarStyleLightContent];
-            
+
             // [chatViewManager setRecordMode:MQRecordModeDuckOther];
             // [chatViewManager setPlayMode:MQPlayModeMixWithOther];
-            
-            
+
+
             // [[UINavigationBar appearance] setTranslucent:NO];
         } else {
             NSLog(@"error:%@",error);
         }
     }];
     [MQManager openMeiqiaService];
-    
-    
+
+
 }
 
 + (NSString*)cordovaVersion
