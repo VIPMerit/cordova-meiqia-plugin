524745ab1d94b57b1fce7a63e786d9bfc712f291
Jakob Cosoroabă <jakob@cosoroaba.ro>
2016-11-02 13:07:04 +0200

    better injection

--- www/MeiQia.js
+++ www/MeiQia.js
@@ -4,11 +4,11 @@ var MeiQia = function(){};
 // cordova.exec(success,failure,"MeiQia",functionName,argumentsArray);   
 
 MeiQia.prototype.init = function(success, failure, appKey){
-    cordova.exec(success, failure, "MeiQia", "init", appKey);
+    cordova.exec(success, failure, "MeiQia", "init", [appKey]);
 };
 
-MeiQia.prototype.init = function(success, failure){
-    cordova.exec(success, failure, "MeiQia", "openChat");
+MeiQia.prototype.openChat = function(success, failure){
+    cordova.exec(success, failure, "MeiQia", "openChat",[]);
 };
 
 //Plug in to Cordova
