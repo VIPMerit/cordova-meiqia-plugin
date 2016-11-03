69a97d81d713121ebd34db9a424154ed7f2e7f48
feredean <tiberiu.feredean@gmail.com>
2016-11-03 16:47:28 +0200

    fixes

--- src/ios/MeiQiaCordovaPlugin.h
+++ src/ios/MeiQiaCordovaPlugin.h
@@ -1,6 +1,6 @@
 #import <Cordova/CDV.h>
 #import <MeiQiaSDK/MeiqiaSDK.h>
-#import "MQChatViewController/Config/MQChatViewManager.h"
+#import "MQChatViewManager.h"
 
 @interface MeiQiaCordovaPlugin : CDVPlugin
 
