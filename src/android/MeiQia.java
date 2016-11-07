e256eb4ea4cbe367123698f393d5017f96c736b8
Jakob Cosoroabă <jakob@cosoroaba.ro>
2016-11-07 18:51:01 +0200

    implement missing hooks

--- src/android/MeiQia.java
+++ src/android/MeiQia.java
@@ -6,15 +6,20 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 import com.meiqia.core.MQManager;
+import com.meiqia.core.callback.OnClientInfoCallback;
 import com.meiqia.core.callback.OnInitCallback;
 import com.meiqia.meiqiasdk.util.MQConfig;
 import com.meiqia.meiqiasdk.util.MQIntentBuilder;
+
+import java.util.HashMap;
+import java.util.Iterator;
 
 
 public class MeiQia extends CordovaPlugin{
@@ -26,6 +31,21 @@ public class MeiQia extends CordovaPlugin{
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         cordovaContext = cordova.getActivity();
+    }
+
+    private HashMap<String, String> buildClientInfo(JSONObject raw) {
+        HashMap<String, String> clientInfo = new HashMap<String, String>();
+
+        Iterator<String> iter = raw.keys();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            try {
+                clientInfo.put(key, raw.getString(key));
+            } catch (JSONException e) {
+                // Something went wrong!
+            }
+        }
+        return clientInfo;
     }
 
     @Override
@@ -47,12 +67,15 @@ public class MeiQia extends CordovaPlugin{
                 });
             }
             if(action.equals("openChat")){
-                final String customId = args.getString(0);
+                JSONObject initArgs = new JSONObject(args.getString(0));
+                final String customId = initArgs.getString("haishangId");
+                final HashMap<String,String> clientInfo = buildClientInfo(initArgs);
                 cordova.getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         MQIntentBuilder mqb = new MQIntentBuilder(cordovaContext);
                         mqb.setCustomizedId(customId);
+                        mqb.setClientInfo(clientInfo);
                         Intent intent = mqb.build();
                         cordova.getActivity().startActivity(intent);
                         callbackContext.success();
@@ -61,19 +84,32 @@ public class MeiQia extends CordovaPlugin{
             }
             if(action.equals("updateClientInfo")){
                 MQManager mqManager = MQManager.getInstance(cordovaContext);
-                 callbackContext.success();
+                HashMap<String,String> clientInfo = buildClientInfo(initArgs);
+                mqManager.updateClientInfo(clientInfo, new OnClientInfoCallback(){
+                    @Override
+                    public void onSuccess() {
+                        callbackContext.success();
+                    }
+
+                    @Override
+                    public void onFailure(int code, String message) {
+                        callbackContext.error(LOG_TAG + ", " + code +", " +message);
+                    }
+                });
             }
             if(action.equals("closeChat")){
                 MQManager mqManager = MQManager.getInstance(cordovaContext);
-                 callbackContext.success();
+                mqManager.closeMeiqiaService();
+                callbackContext.success();
             }
             if(action.equals("setOffline")){
                 MQManager mqManager = MQManager.getInstance(cordovaContext);
+                mqManager.setClientOffline();
                 callbackContext.success();
             }
         }
         catch (Exception e) {
-            Log.e(LOG_TAG,"exception while perfroming action:"+action,e);
+            Log.e(LOG_TAG,"exception while performing action:"+action,e);
             callbackContext.error("exception while performing action"+action);
         }
         return true;
